77dcd07dc6474db9b639e6655d6d98f25ef8da82
Felipe <luisf.munera@udea.edu.co>
2019-07-13 11:07:11 -0500

    sin errores pero aun no funciona

---
+++ src/components/TranslateIn.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="TranslateIn">
+    Translation
+    <form v-on:submit="formSubmit">
+      <input type="text" v-model="textToTranslate" placeholder="Write the text to translate"/>
+      <input type="submit" value="Translate">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TranslateIn",
+  data() {
+    return {
+      textToTranslate: "",
+      languaje: ""
+    };
+  },
+  created() {
+    this.languaje = "en";
+  },
+  methods: {
+    formSubmit(e) {
+      this.$emit("formSubmit", this.textToTranslate, this.languaje);
+      e.preventDefault();
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
